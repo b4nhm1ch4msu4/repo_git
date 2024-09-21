b6ac7794bb4c11b4ab8a3b8f0c2c1f48d6fd9f27
arch_wsl_workNB <tienpham102vp@gmail.com>
2024-09-21 13:58:24 +0700

    work_nb

--- cpp/lab/wwf.cpp
+++ cpp/lab/wwf.cpp
@@ -5,28 +5,28 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  cout << "Working with file" << "\n";
+  /*cout << "Working with file" << "\n";*/
 
   fstream myfile;
-  myfile.open("text.txt", ios::out);
-  if(myfile.is_open()){
-    myfile << "hello world \n";
-    myfile << "second line \n";
-    myfile.close();
-  }
-
-  myfile.open("text.txt", ios::app);
-  if(myfile.is_open()){
-    myfile << "hello second time \n";
-    myfile.close();
-  }
+  /*myfile.open("text.txt", ios::out);*/
+  /*if(myfile.is_open()){*/
+  /*  myfile << "hello world \n";*/
+  /*  myfile << "second line \n";*/
+  /*  myfile.close();*/
+  /*}*/
+  /**/
+  /*myfile.open("text.txt", ios::app);*/
+  /*if(myfile.is_open()){*/
+  /*  myfile << "hello second time \n";*/
+  /*  myfile.close();*/
+  /*}*/
 
 
   myfile.open("text.txt", ios::in);
   if(myfile.is_open()){
-    cout << "reading from a file .\n";
+    /*cout << "reading from a file .\n";*/
     string line;
-    while (getline(myfile, line)) {
+    while (getline(myfile, line, ' ')) {
       cout << line << endl;
     }
     myfile.close();
--- cpp/login_form/main.cpp
+++ cpp/login_form/main.cpp
@@ -1,29 +1,37 @@
-#include <iostream> 
-#include <ostream>
-#include <string> 
+#include <cstdlib>
+#include <filesystem>
 #include <fstream>
-/*#include <termios.h>*/
+#include <iostream>
+#include <ostream>
+#include <string>
 #include <unistd.h>
+#define FILENAME "data.txt"
 
 using namespace std;
 
 /**
  * display menu options
-*/
-void display_menu(){
-  cout << "LOGIN FORM - WEBCOME PAGE!!!\n";
+ */
+void display_menu() {
+  cout << "----------------------------------\n";
+  cout << "---LOGIN FORM - WEBCOME PAGE!!!---\n";
+  cout << "----------------------------------\n";
   cout << "\n";
   cout << "1. Login\n";
   cout << "2. Sign-up\n";
   cout << "3. Reset password\n";
+  cout << "4. Quit\n";
+
+  cout << "Enter your option ::";
 }
 
 /**
- * get username and password
- * @para 
- * @return string "[username] [password]"
+ * ---------------
+ * ----Login------
+ * ---------------
+ * Get username and password from input and compare with records in data.txt
  */
-int login(){
+int login() {
   string username, pass;
   cout << "LOGIN PAGE\n";
   cout << "Please enter username and password!\n";
@@ -33,11 +41,11 @@ int login(){
   pass = getpass("Password: ");
   string login_info{username + " " + pass};
   fstream file_data;
-  file_data.open("data.txt", ios::in);
-  if(file_data.is_open()){
+  file_data.open(FILENAME, ios::in);
+  if (file_data.is_open()) {
     string line;
     while (getline(file_data, line)) {
-      if(line.find(login_info) != string::npos){
+      if (line.find(login_info) != string::npos) {
         cout << "Login Success\nWelcome " << username << endl;
         file_data.close();
         return 0;
@@ -52,10 +60,13 @@ int login(){
 }
 
 /**
- * Create new user and add to data.txt
+ * ---------------
+ * ----Sign-up----
+ * ---------------
+ * Get username and password from input and add new record to data.txt
  */
 
-int sign_up(){
+int sign_up() {
   string username, pass;
   cout << "SIGNUP PAGE\n";
   cout << "Create new user!\n";
@@ -64,11 +75,11 @@ int sign_up(){
   cin >> username;
   // check if username is exist
   fstream file_data;
-  file_data.open("data.txt",ios::in);
-  if(file_data.is_open()){
+  file_data.open(FILENAME, ios::in);
+  if (file_data.is_open()) {
     string line;
-    while(getline(file_data, line)){
-      if(line.find(username) != string::npos){
+    while (getline(file_data, line)) {
+      if (line.find(username) != string::npos) {
         cout << "Username is exist !!!\n";
         cout << "Sign-up fail !\n";
         file_data.close();
@@ -80,18 +91,18 @@ int sign_up(){
 
   pass = getpass("Password: ");
   string re_type_pass{getpass("Re-type password: ")};
-  if (pass != re_type_pass){
+  if (pass != re_type_pass) {
     cout << "Password not match !!!\n";
     cout << "Sign-up fail!\n";
-  }else {
-    file_data.open("data.txt", ios::app);
-    if(file_data.is_open()){
+  } else {
+    file_data.open(FILENAME, ios::app);
+    if (file_data.is_open()) {
       file_data << username << " " << pass << "\n";
       cout << "Sign-up success !!!\n";
       cout << "New user " << username << " was added.\n";
       file_data.close();
       return 0;
-    }else {
+    } else {
       cout << "Could not open data file !\n";
     }
     file_data.close();
@@ -100,43 +111,69 @@ int sign_up(){
 }
 
 /**
- * Reset password
+ * ---------------
+ * ----Reset------
+ * ---------------
+ * Copy all records in data.txt to temp.txt, excep record contains username
+ * rename temp.txt -> data.txt
  * */
-int reset_pass(){
+int reset_pass() {
   string username;
   cout << "Reset Password !!!\n";
-  cout << "Enter your username: \n";
+  cout << "Enter your username: ";
   cin >> username;
-  fstream file_data;
-  file_data.open("data.txt", ios::in);
 
-  if(file_data.is_open()){
-    /*string line;*/
-    /*while (getline(file_data, line)) {*/
-    /**/
-    /*}*/
+  // remove username from data
+  ifstream file_data{FILENAME};
+  if (!file_data.is_open()) {
+    cout << "Can't open file data. ERROR ERROR ERROR!!!\n";
+    return 1;
   }
+  ofstream temp_data{"temp.txt"};
+  if (!temp_data.is_open()) {
+    cout << "Can't create temp file data. ERROR ERROR ERROR!!!\n";
+    return 1;
+  }
+
+  string line;
+  while (getline(file_data, line)) {
+    if (line.find(username) == string::npos) {
+      temp_data << line << "\n";
+    }
+  }
+  file_data.close();
+  temp_data.close();
+
+  filesystem::remove(FILENAME);
+  filesystem::rename("temp.txt", FILENAME);
+
+  cout << "Your account was deleted. Please create new one !\n";
+
   return 0;
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+  bool quit = false;
+
   display_menu();
+
   int option;
   cin >> option;
   switch (option) {
-    case 1:
-      login();
-      break;
-    case 2:
-      sign_up();
-      break;
-    case 3:
-      reset_pass();
-      cout << "reset password\n";
-      break;
-    default:
-      cout << "Invalid option !!";
+  case 1:
+    login();
+    break;
+  case 2:
+    sign_up();
+    break;
+  case 3:
+    reset_pass();
+    break;
+  case 4:
+    quit = true;
+    break;
+  default:
+    cout << "Invalid option !!";
   }
   return 0;
-
 }
