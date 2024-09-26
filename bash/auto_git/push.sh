f1c52caff6d9573a1790321f99517e7924daf3a5
arch_wsl_workNB <tienpham102vp@gmail.com>
2024-09-26 15:18:04 +0700

    work_nb

---
+++ bash/auto_git/push.sh
@@ -0,0 +1,11 @@
+#!/usr/sbin/bash
+
+cd ~/e/Documents/__Obsidian_home || exit
+git add .
+git commit -m "WSL_Arch_workNB"
+git push
+
+cd ~/e/repo_git/ || exit
+git add.
+git commit -m "WSL_Arch_workNB"
+git push
---
+++ cpp/qt_projects/AnalogClock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AnalogClock_FILE E:/repo_git/cpp/qt_projects/AnalogClock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/AnalogClock.exe)
+set(__QT_DEPLOY_TARGET_AnalogClock_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AnalogClock_RUNTIME_DLLS E:/Programs/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;E:/Programs/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;E:/Programs/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
