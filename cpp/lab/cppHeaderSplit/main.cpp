a6a3b857b904fee238d2bef43b361c7a9821645a
Home_PC_arch_btw <tienpham102vp@gmail.com>
2024-09-27 22:54:24 +0700

    home_pc

---
+++ cpp/lab/cppHeaderSplit/main.cpp
@@ -0,0 +1,6 @@
+#include "include/print.h"
+
+int main(int argc, char *argv[]) {
+  print("hello world");
+  return 0;
+}
---
+++ cpp/lab/cppHeaderSplit/src/print.cpp
@@ -0,0 +1,3 @@
+#include "../include/print.h"
+
+void print(const char *message) { std::cout << message << std::endl; }
