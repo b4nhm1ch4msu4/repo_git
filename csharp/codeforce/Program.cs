0bb6403b54152f042a542b74eb7f2a19f1c0b9d1
arch_wsl_workNB <tienpham102vp@gmail.com>
2024-12-11 16:55:05 +0700

    WSL_Arch_workNB

---
+++ csharp/codeforce/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeForce;
+  class Program {
+    static void Main(){
+      int testCases = Convert.ToInt32(Console.ReadLine());
+      for (int i = 0; i < testCases; i++){
+        int days = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[days];
+        arr = Console.ReadLine().Split().Select(str => int.Parse(str)).ToArray();
+        if (days < 3){
+          Console.WriteLine(days);
+          continue;
+        }
+        int leng = arr.Length;
+        int sum = 0;
+        int need = 8;
+        int res = 1;
+        for (int k = 1; k < leng; k++){
+          sum += arr[k];
+          while (need < sum){
+            need = need * 2 + 4;
+          }
+          if (sum == need ){
+            res += 1;
+            need = need * 2 + 4;
+          }
+        }
+        Console.WriteLine(res);
+      }
+    }
+  }
+
