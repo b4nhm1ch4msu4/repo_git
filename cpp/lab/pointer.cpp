81683cec6830f2856ef1224ff8ef50a9f401e4ae
arch_wsl_workNB <tienpham102vp@gmail.com>
2024-12-27 17:29:42 +0700

    work-nb

--- cpp/lab/pointer.cpp
+++ cpp/lab/pointer.cpp
@@ -28,13 +28,21 @@ using namespace std;
 void resize(int *arr, int newSize) {
   // allocate arr to new pointer*/
   /*delete[] arr;*/
+  /*cout << arr << endl;*/
   arr = new int[newSize];
+  /*cout << arr << endl;*/
+  /*arr = new int[newSize + 10];*/
+  /*cout << "address of arr: " << arr << "\n";*/
 }
 
 int main() {
   int *arr = new int[1];
   cout << arr << endl;
+  for (int i = 0; i < 10; i++) {
+    cout << arr[i];
+  }
   resize(arr, 10);
+  /*cout << &arr[0] << "\n";*/
   cout << arr << endl;
   delete[] arr;
   return 0;
