2810ecc7a92d11d13b02961332adfe2bac4a62b1
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-01-14 16:55:06 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/check.cpp
+++ cpp/cpp_tutorial/check.cpp
@@ -1,7 +1,15 @@
-#define MYNAME "tienpham"
+#include <iostream>
 
-/*int main(int argc, char *argv[]) {*/
-/*  std::cout << "my name is " << MYNAME;*/
-/**/
-/*  return 0;*/
-/*}*/
+int main()
+{
+    std::cout << "Input a keyboard character: "; // assume the user enters "a b" (without quotes)
+
+    char ch{};
+    std::cin >> ch; // extracts a, leaves " b\n" in stream
+    std::cout << "You entered: " << ch << '\n';
+
+    std::cin >> ch; // extracts space, leaves "b\n" in stream
+    std::cout << "You entered: " << ch << '\n';
+
+    return 0;
+}
