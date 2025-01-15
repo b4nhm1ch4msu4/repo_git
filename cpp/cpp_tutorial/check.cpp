332d8376b9ff15545b36bdb1b4ea8b1ee4f61b0c
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-01-15 16:58:28 +0700

    work-nb

--- cpp/cpp_tutorial/check.cpp
+++ cpp/cpp_tutorial/check.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Input a keyboard character: "; // assume the user enters "a b" (without quotes)
-
-    char ch{};
-    std::cin >> ch; // extracts a, leaves " b\n" in stream
-    std::cout << "You entered: " << ch << '\n';
-
-    std::cin >> ch; // extracts space, leaves "b\n" in stream
-    std::cout << "You entered: " << ch << '\n';
-
-    return 0;
+int main() {
+  std::vector<int> int_vec = {};
+  std::cout << int_vec.size() << std::endl;
+  std::cout << int_vec[0];
+  return 0;
 }
