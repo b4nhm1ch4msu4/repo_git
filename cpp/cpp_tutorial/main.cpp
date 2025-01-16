a7615cb85cc544fba24881acda17a19243232684
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-01-16 16:55:06 +0700

    WSL_Arch_workNB

---
+++ cpp/cpp_tutorial/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <istream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  int age_1{};
+  int age_2{};
+  std::string name_1{};
+  std::string name_2{};
+  std::cout << "Enter your name 1: ";
+  std::getline(std::cin >> std::ws, name_1);
+  std::cout << "Enter your age 1: ";
+  std::cin >> age_1;
+
+  std::cout << "Enter your name 2: ";
+  std::getline(std::cin >> std::ws, name_2);
+  std::cout << "Enter your age 2: ";
+  std::cin >> age_2;
+  if (age_1 > age_2) {
+    std::cout << name_1 << " (age " << age_1 << ") is older than " << name_2
+              << " (age " << age_2 << ").";
+  } else {
+    std::cout << name_2 << " (age " << age_2 << ") is older than " << name_1
+              << " (age " << age_1 << ").";
+  }
+
+  return 0;
+}
