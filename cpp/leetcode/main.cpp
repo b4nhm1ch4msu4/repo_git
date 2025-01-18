121ec44acee7b314d3f584acd4f6e1dcdef5cf31
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-01-18 16:52:04 +0700

    work-nb

--- cpp/leetcode/main.cpp
+++ cpp/leetcode/main.cpp
@@ -1,33 +1,11 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <algorithm>
+#include <iterator>
+#include <string>
 
 class Solution {
 public:
-  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
-    int midx = m - 1;
-    int nidx = n - 1;
-    int right = m + n - 1;
-    while (nidx >= 0) {
-      if (midx >= 0 && nums1[midx] > nums2[nidx]) {
-        nums1[right] = nums1[midx];
-        midx--;
-      } else {
-        nums1[right] = nums2[nidx];
-        nidx--;
-      }
-      right--;
-    }
+  bool isPalindrome(int x) {
+    const std::string str = std::to_string(x);
+    return std::equal(std::begin(str), std::end(str), std::rbegin(str));
   }
 };
-
-int main(int argc, char *argv[]) {
-  Solution sol = Solution();
-  vector<int> nums1{1, 2, 3, 0, 0, 0};
-  vector<int> nums2{2, 4, 5};
-  sol.merge(nums1, 3, nums2, 3);
-  for (int item : nums1) {
-    cout << item << endl;
-  }
-  return 0;
-}
