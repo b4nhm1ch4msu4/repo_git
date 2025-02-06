7a1294c2a99da1bb432fb0da086998a7f8acfc32
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-02-06 16:55:05 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 
-void print(int num) {
-  std::cout << "int function print number: " << num << '\n';
-}
-
-void print(double num) {
-  std::cout << "int function print double: " << num << '\n';
-}
+template <typename T, typename Y> auto sub(T a, Y b) { return a - b; }
 
 int main() {
-  float num = 10999;
-  print('c');
-  print(num);
+  std::cout << sub(2, 3) << '\n';
+  std::cout << sub(1.2, 3) << '\n';
+  std::cout << sub(3, 2) << '\n';
+  std::cout << sub(3.5, 2) << '\n';
+  std::cout << sub(4, 1.5) << '\n';
 }
