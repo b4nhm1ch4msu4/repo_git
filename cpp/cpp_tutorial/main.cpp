4be64bf632cd9647026140114b08fff5844cc732
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-02-13 16:55:05 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,13 +1,28 @@
+#include <algorithm>
 #include <iostream>
+
+using namespace std;
+
+class Student
+{
+private:
+  string name;
+  int age;
+
+public:
+  Student(string name, int age)
+  {
+    static int count = 0;
+    this->name = name;
+    this->age = age;
+    count++;
+  }
+};
+
 int main()
 {
-  int x{5};           // non-const
-  int* const ptr{&x}; // ptr points to a "const int"
-  std::cout << *ptr << '\n';
-
-  /**ptr = 6;*/
-  x = 6; // allowed: the value is still non-const when accessed through
-  std::cout << *ptr << '\n';
-
+  Student s1("Manh", 19);
+  Student s2("Trung", 19);
+  cout << Student::numberOfStudents << '\n';
   return 0;
 }
