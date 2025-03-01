21f72ef27b0bb7e355a3afb39f6f84f7a330c756
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-03-01 16:59:02 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/check.cpp
+++ cpp/cpp_tutorial/check.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
-struct Point3d
+class Foo
 {
-  double m_x;
-  double m_y;
-  double m_z;
+public:
+  Foo(int x) : m_value{x} { std::cout << "Foo constructor\n"; }
+  ~Foo() { std::cout << "Foo destructor\n"; }
 
-  /*public:*/
-  /*  void setValues(double x, double y, double z)*/
-  /*  {*/
-  /*    m_x = x;*/
-  /*    m_y = y;*/
-  /*    m_z = z;*/
-  /*  }*/
-  void print() const { std::cout << m_x << ", " << m_y << ", " << m_z << '\n'; }
-  /*  bool isEqual(const Point3d& other) const*/
-  /*  {*/
-  /*    return m_x == other.m_x && m_y == other.m_y && m_z == other.m_z;*/
-  /*  }*/
+private:
+  int m_value;
 };
 
 int main(int argc, char* argv[])
 {
-  Point3d point1{1, 2, 3};
-  point1.print();
+  Foo f{1};
   return 0;
 }
