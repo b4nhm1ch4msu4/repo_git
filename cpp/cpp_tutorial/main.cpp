3853a295e9532d224bf5280988cfca8f89067c53
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-03-14 16:55:01 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,42 +1,12 @@
-#include <chrono>
 #include <iostream>
-#include <random>
+#include <vector>
 
-namespace Random {
-inline std::mt19937 generate() {
-  std::random_device rd{};
-
-  // Create seed_seq with high-res clock and 7 random numbers from
-  // std::random_device
-  std::seed_seq ss{
-      static_cast<std::seed_seq::result_type>(
-          std::chrono::steady_clock::now().time_since_epoch().count()),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd()};
-
-  return std::mt19937{ss};
-}
-
-inline std::mt19937 mt{generate()};  // generates a seeded std::mt19937 and
-                                     // copies it into our global object
-
-// Generate a random int between [min, max] (inclusive)
-inline int get(int min, int max) {
-  return std::uniform_int_distribution{min, max}(mt);
-}
-}  // namespace Random
-
-int main() {
-  // Print a bunch of random numbers
-  for (int count{1}; count <= 10; ++count)
-    std::cout << Random::get(1, 6) << '\t';
-
-  std::cout << '\n';
-
+int main(int argc, char *argv[]) {
+  std::vector<int> v(3);
+  for (int i = 0; i < v.size(); i++) {
+    std::cin >> v[i];
+  }
+  std::cout << v[0] + v[1] + v[2] << '\n';
+  std::cout << v[0] * v[1] * v[2] << '\n';
   return 0;
 }
