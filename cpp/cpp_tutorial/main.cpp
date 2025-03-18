201362e5ee5f7fd985a6dc396734698bd8d5a506
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-03-18 16:55:05 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,16 +1,14 @@
+#include <cassert>
 #include <cstddef>
 #include <iostream>
 #include <vector>
 
-template <typename T>
-void printArray(const std::vector<T> &arr) {
-  for (std::size_t i = 0; i < arr.size(); ++i)
-    std::cout << arr[i] << ' ';
-  std::cout << std::endl;
+namespace Animal {
+enum { chicken, dog, cat, elephant, duck, snake, total };
 }
 
 int main(int argc, char *argv[]) {
-  std::vector arr{4, 6, 7, 3, 8, 2, 1, 9};
-  printArray(arr);
-  return 0;
+  std::vector<int> legs{2, 4, 4, 4, 2, 0};
+  assert(Animal::total == legs.size());
+  std::cout << "elephant has " << legs[Animal::elephant] << " legs\n";
 }
