89abeb8d75bc2184e4cd1eea1401ebe1dbfce34d
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-03-19 16:55:05 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,14 +1,8 @@
-#include <cassert>
 #include <cstddef>
 #include <iostream>
 #include <vector>
 
-namespace Animal {
-enum { chicken, dog, cat, elephant, duck, snake, total };
-}
-
 int main(int argc, char *argv[]) {
-  std::vector<int> legs{2, 4, 4, 4, 2, 0};
-  assert(Animal::total == legs.size());
-  std::cout << "elephant has " << legs[Animal::elephant] << " legs\n";
+  std::vector numbers{0, 2, 4, 6, 8, 10};
+  const std::vector doubles{1.2, 3.4, 5.6, 7.8};
 }
