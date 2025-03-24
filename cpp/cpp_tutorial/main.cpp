c09d71d6f42bb3e5594feda34962863493bc1328
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-03-24 16:55:06 +0700

    WSL_Arch_workNB

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-  cout << "Hello World";
+int main(int argc, char* argv[]) {
+  constexpr int perfect_square[]{0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
+  while (true) {
+    bool found = false;
+    std::cout << "Enter a single digit integer, or -1 to quit: ";
+    int input;
+    std::cin >> input;
+    if (input == -1)
+      break;
+    for (auto i : perfect_square) {
+      if (input == i) {
+        found = true;
+        std::cout << "It's a perfect square\n";
+      }
+    }
+    if (!found) {
+      std::cout << "It's not a perfect square\n";
+    }
+  }
   return 0;
 }
