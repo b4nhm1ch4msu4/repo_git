639672dde1c954c63f10ee24ec48989cfc251bc3
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-04-08 16:55:05 +0700

    WSL_Arch_workNB

---
+++ cpp/cpp_tutorial/Array2d.cpp
@@ -0,0 +1,33 @@
+#include <array>
+#include <iostream>
+
+template <typename T, std::size_t Row, std::size_t Col>
+using Array2d = std::array<std::array<T, Col>, Row>;
+
+template <typename T, std::size_t Row, std::size_t Col>
+constexpr int rowLength(const Array2d<T, Row, Col>&) {
+  return Row;
+}
+
+template <typename T, std::size_t Row, std::size_t Col>
+constexpr int colLength(const Array2d<T, Row, Col>&) {
+  return Col;
+}
+
+int main(int argc, char* argv[]) {
+  Array2d<int, 4, 3> my_2dArray = {{
+      {1, 2, 3},
+      {1, 2, 3},
+      {1, 2, 3},
+      {1, 2, 3},
+  }};
+  std::cout<< "array row: " << rowLength(my_2dArray) << "\n";
+  std::cout<< "array col: " << colLength(my_2dArray) << "\n";
+  for(auto& row:my_2dArray){
+    for(auto& col:row){
+      std::cout << col << ' ';
+    }
+    std::cout << "\n";
+  }
+  return 0;
+}
--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
+#include <cstring>
+
+void print(const char str[]){
+  const char *ptr{str};
+  while(*ptr != '\0'){
+    ++ptr;
+  }
+  while (ptr-- != str) {
+    std::cout << *ptr;
+  }
+}
 
 int main(int argc, char* argv[]) {
-  constexpr int perfect_square[]{0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
-  while (true) {
-    bool found = false;
-    std::cout << "Enter a single digit integer, or -1 to quit: ";
-    int input;
-    std::cin >> input;
-    if (input == -1)
-      break;
-    for (auto i : perfect_square) {
-      if (input == i) {
-        found = true;
-        std::cout << "It's a perfect square\n";
-      }
-    }
-    if (!found) {
-      std::cout << "It's not a perfect square\n";
-    }
-  }
+  char str[]{"hello world"};
+  print(str);
   return 0;
+
 }
