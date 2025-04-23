6cba0aea8971931ea5044af1445ef1933e600119
mike <tienpham102vp@gmail.com>
2025-04-23 16:50:01 +0700

    Work-nb

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,60 +1,24 @@
-#include <functional>
 #include <iostream>
-#include <limits>
 
-int get_number() {
-  int num{};
-  std::cout << "Enter a number: ";
-  std::cin >> num;
-  return num;
-}
-
-char get_operator() {
-  char op{};
-  do {
-    std::cout << "Choose operator: + , - , * , /: ";
-    std::cin >> op;
-  } while (op != '+' && op != '-' && op != '*' && op != '/');
-  return op;
-}
-
-int add(int a, int b) {
-  return a + b;
-}
-
-int subtract(int a, int b) {
-  return a - b;
-}
-
-int multiply(int a, int b) {
-  return a * b;
-}
-
-int divide(int a, int b) {
-  return a / b;
-}
-
-using ArithmeticFunction = std::function<int(int, int)>;
-
-ArithmeticFunction getArithmeticFunction(char op) {
-  switch (op) {
-    case '+':
-      return add;
-    case '-':
-      return subtract;
-    case '*':
-      return multiply;
-    case '/':
-      return divide;
-    default:
-      return nullptr;
+int factorial(int a){
+  if(a == 1){
+    return 1;
   }
+  return a * factorial(a-1);
 }
 
-int main(int argc, char* argv[]) {
-  int a{get_number()};
-  char op{get_operator()};
-  int b{get_number()};
-  std::cout << getArithmeticFunction(op)(a, b) << std::endl;
+int individual_digit_sum(int a){
+  if(a == 0){
+    return 0;
+  }
+  return a % 10 + individual_digit_sum(a / 10);
+}
+
+int main (int argc, char *argv[]) {
+  std::cout << "Enter a number: ";
+  int num{};
+  std::cin >> num;
+  std::cout << "Factorial: " << factorial(num) << std::endl;
+  std::cout << "Sum of digits: " << individual_digit_sum(num) << std::endl;
   return 0;
 }
---
+++ cpp/cpp_tutorial/simple-calculator.cpp
@@ -0,0 +1,59 @@
+#include <functional>
+#include <iostream>
+
+int get_number() {
+  int num{};
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+  return num;
+}
+
+char get_operator() {
+  char op{};
+  do {
+    std::cout << "Choose operator: + , - , * , /: ";
+    std::cin >> op;
+  } while (op != '+' && op != '-' && op != '*' && op != '/');
+  return op;
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int subtract(int a, int b) {
+  return a - b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int divide(int a, int b) {
+  return a / b;
+}
+
+using ArithmeticFunction = std::function<int(int, int)>;
+
+ArithmeticFunction getArithmeticFunction(char op) {
+  switch (op) {
+    case '+':
+      return add;
+    case '-':
+      return subtract;
+    case '*':
+      return multiply;
+    case '/':
+      return divide;
+    default:
+      return nullptr;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  int a{get_number()};
+  char op{get_operator()};
+  int b{get_number()};
+  std::cout << getArithmeticFunction(op)(a, b) << std::endl;
+  return 0;
+}
