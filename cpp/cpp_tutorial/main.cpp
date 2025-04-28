fdb4cfa733efaf17e696c2d9a270df5c79818580
mike <tienpham102vp@gmail.com>
2025-04-28 16:50:04 +0700

    Work-nb

--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,52 +1,39 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Digit {
- private:
-  int m_digit{};
-
- public:
-  Digit(int digit = 0) : m_digit{digit} {}
-
-  void operator++();
-  Digit& operator--();
-
-  friend std::ostream& operator<<(std::ostream& out, const Digit& d);
+struct StudentGrade {
+  std::string name{};
+  char grade{};
 };
 
-void Digit::operator++() {
-  // If our number is already at 9, wrap around to 0
-  if (m_digit == 9)
-    m_digit = 0;
-  // otherwise just increment to next number
-  else
-    ++m_digit;
+class GradeMap {
+  std::vector<StudentGrade> m_map{};
 
-}
+ public:
+  char& operator[](std::string name) {
+    auto found = std::find_if(m_map.begin(), m_map.end(),
+                              [&](StudentGrade& student) { return student.name == name; });
 
-Digit& Digit::operator--() {
-  // If our number is already at 0, wrap around to 9
-  if (m_digit == 0)
-    m_digit = 9;
-  // otherwise just decrement to next number
-  else
-    --m_digit;
+    if (found != m_map.end())
+      return found->grade;
 
-  return *this;
-}
-
-std::ostream& operator<<(std::ostream& out, const Digit& d) {
-  out << d.m_digit;
-  return out;
-}
+    m_map.push_back({
+        name,
+    });
+    return m_map.back().grade;
+  }
+};
 
 int main() {
-  Digit digit{8};
+  GradeMap grades{};
 
-  std::cout << digit;
-  std::cout << ++digit;
-  std::cout << ++digit;
-  std::cout << --digit;
-  std::cout << --digit;
+  grades["Joe"] = 'A';
+  grades["Frank"] = 'B';
+
+  std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+  std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
 
   return 0;
 }
