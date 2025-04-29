2cbdf1e6a10bff28b020cc7e4628be6668a81bdf
mike <tienpham102vp@gmail.com>
2025-04-29 16:50:11 +0700

    Work-nb

--- cpp/code_comp/code_force/main.cpp
+++ cpp/code_comp/code_force/main.cpp
@@ -4,26 +4,30 @@
 #include <string>
 
 int main() {
-  std::string input1, input2;
-
-  std::getline(std::cin, input1);
-  std::getline(std::cin, input2);
-
-  // Convert to lowercase
-  std::transform(input1.begin(), input1.end(), input1.begin(),
-                 [](unsigned char c) { return std::tolower(c); });
-  std::transform(input2.begin(), input2.end(), input2.begin(),
-                 [](unsigned char c) { return std::tolower(c); });
-
-  // Compare the strings
-  if (input1 == input2)
-    std::cout << "0\n";
-  else if (input1 < input2)
-    std::cout << "-1\n";
-  else
-    std::cout << "1\n";
-
-  // Output the result
-
+  int testcases{};
+  std::cin >> testcases;
+  while (testcases--) {
+    int n;
+    char ch[100];
+    std::cin >> n >> ch;
+    int min_num{1};
+    int max_num{n};
+    int result[100]{};
+    for (int j{n - 2}; j >= 0; j--) {
+      if (ch[j] == '>') {
+        result[j + 1] = max_num;
+        --max_num;
+      }
+      if (ch[j] == '<') {
+        result[j + 1] = min_num;
+        ++min_num;
+      }
+    }
+    result[0] = min_num;
+    for (int j{0}; j < n; j++) {
+      std::cout << result[j] << " ";
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
--- cpp/cpp_tutorial/main.cpp
+++ cpp/cpp_tutorial/main.cpp
@@ -1,21 +1,85 @@
+#include <cassert>
 #include <iostream>
-#include <string>
+#include <ostream>
 
-class MyString {
-  std::string str{};
+class IntArray {
+ private:
+  int* m_array{};
+  int m_size{};
 
  public:
-  MyString(std::string s) : str{s} {}
-  friend std::ostream& operator<<(std::ostream& out, const MyString& s) { return out << s.str; }
-  std::string operator()(std::size_t index, std::size_t length) const {
-    return str.substr(index, length);
+  IntArray(int size) {
+    assert(size > 0);
+    m_size = size;
+    m_array = new int[size];
+  }
+
+  IntArray(const IntArray& other) { deep_copy(other); }
+
+  ~IntArray() {
+    delete[] m_array;
+    m_array = nullptr;
+  }
+
+  void deep_copy(const IntArray& other) {
+    delete[] m_array;
+    m_size = other.m_size;
+    m_array = new int[m_size];
+    if (other.m_array) {
+      for (int i = 0; i < m_size; i++) {
+        m_array[i] = other.m_array[i];
+      }
+    } else {
+      m_array = nullptr;
+    }
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const IntArray& arr) {
+    for (int i = 0; i < arr.m_size; i++) {
+      out << arr.m_array[i] << " ";
+    }
+    return out;
+  }
+
+  int& operator[](int index) {
+    assert(index >= 0 && index < m_size);
+    return m_array[index];
+  }
+
+  IntArray& operator=(const IntArray& other) {
+    if (this != &other) {
+      deep_copy(other);
+    }
+    return *this;
   }
 };
 
-int main()
-{
-    MyString s { "Hello, world!" };
-    std::cout << s(7, 5) << '\n'; // start at index 7 and return 5 characters
+IntArray fillArray() {
+  IntArray a(5);
 
-    return 0;
+  a[0] = 5;
+  a[1] = 8;
+  a[2] = 2;
+  a[3] = 3;
+  a[4] = 6;
+
+  return a;
+}
+
+int main() {
+  IntArray a{fillArray()};
+
+  std::cout << a << '\n';
+
+  auto& ref{a};  // we're using this reference to avoid compiler self-assignment errors
+  a = ref;
+
+  IntArray b(1);
+  b = a;
+
+  a[4] = 7;
+
+  std::cout << b << '\n';
+
+  return 0;
 }
