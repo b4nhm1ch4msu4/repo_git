6fd4b897a57db75b3e9d43bdace48be02e8bb3d4
Laptop HP <tienpham102vp@gmail.com>
2026-02-03 22:57:43 +0700

    update bookbot

--- python/bookbot_bootdev/main.py
+++ python/bookbot_bootdev/main.py
@@ -1,12 +1,12 @@
-from stats import get_book_text, count_words, get_appear_time
+from stats import get_book_text, count_words, get_appear_times, sort_char_dict
 
 
 def main():
     book_filepath = "./books/frankenstein.txt"
     book_content = get_book_text(book_filepath)
-    char_dict = get_appear_time(book_content)
+    char_dict = get_appear_times(book_content)
     # count_words(book_content)
-    print(char_dict)
+    sort_char_dict(char_dict)
 
 
 if __name__ == "__main__":
--- python/bookbot_bootdev/stats.py
+++ python/bookbot_bootdev/stats.py
@@ -10,13 +10,25 @@ def count_words(content: str) -> str:
     print(f"Found {number_of_word} total words")
 
 
-def get_appear_time(content: str) -> dict:
+def get_appear_times(content: str) -> dict:
     char_dict = {}
     word_list = str.split(content)
     for word in word_list:
         word = word.lower()
         for c in word:
-            if char_dict[c] is None:
+            if c not in char_dict.keys():
                 char_dict[c] = 1
-            char_dict[c] += 1
+            else:
+                char_dict[c] = char_dict[c] + 1
     return char_dict
+
+
+def sort_char_dict(char_dict: dict):
+    def sort_on(key):
+        return char_dict[key]
+    key_list = []
+    for k in char_dict.keys():
+        key_list.append(k)
+    key_list.sort(reverse=True, key=sort_on)
+    for c in key_list:
+        print(f"{c}: {char_dict[c]}")
