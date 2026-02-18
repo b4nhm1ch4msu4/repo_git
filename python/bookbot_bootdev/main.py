0cbed505cd85a519c8e77bad9e94d900f4045fc9
Laptop HP <tienpham102vp@gmail.com>
2026-02-19 02:28:44 +0700

    bookbot_bootdev add books for more examples

--- python/bookbot_bootdev/main.py
+++ python/bookbot_bootdev/main.py
@@ -1,8 +1,10 @@
 from stats import get_book_text, count_words, get_appear_times, sort_char_dict
+import sys
 
 
 def main():
-    book_filepath = "./books/frankenstein.txt"
+
+    book_filepath = sys.argv[1]
     book_content = get_book_text(book_filepath)
     char_dict = get_appear_times(book_content)
     # count_words(book_content)
