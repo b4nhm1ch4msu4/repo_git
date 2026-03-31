8b7b1783b8add5f71f42e533e345c4c98690aac7
Laptop HP <tienpham102vp@gmail.com>
2026-03-31 08:49:10 +0700

    tien2.pham add verbose arg

--- projects/boot_dev/mini_claud/main.py
+++ projects/boot_dev/mini_claud/main.py
@@ -1,5 +1,40 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("Gemini api key is None!!!")
+
+client = genai.Client(api_key=api_key)
+
+
+def read_response(respond):
+    print(f"Respond: {respond.text}")
+    if respond.usage_metadata is None:
+        raise RuntimeError("Failed API request")
+    print(f"Prompt tokens: {respond.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {respond.usage_metadata.candidates_token_count}")
+
+
 def main():
-    print("Hello from mini-claud!")
+    parser = argparse.ArgumentParser(description="Mini Claude Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    res = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if (args.verbose):
+        read_response(res)
+    print(res.text)
 
 
 if __name__ == "__main__":
