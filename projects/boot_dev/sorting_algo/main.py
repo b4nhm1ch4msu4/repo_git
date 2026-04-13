16ca1a5b9893aecdb769384bc6749618256d131e
Laptop HP <tienpham102vp@gmail.com>
2026-04-13 08:53:56 +0700

    add sorting_algo folder

---
+++ projects/boot_dev/sorting_algo/main.py
@@ -0,0 +1,41 @@
+# class Influencer:
+#     def __init__(self, num_selfies, num_bio_links):
+#         self.num_selfies = num_selfies
+#         self.num_bio_links = num_bio_links
+#
+#     def __repr__(self):
+#         return f"({self.num_selfies}, {self.num_bio_links})"
+#
+#
+# # dont touch above this line
+#
+#
+# def vanity(influencer):
+#     return influencer.num_bio_links * 5 + influencer.num_selfies
+#
+#
+# def vanity_sort(influencers):
+#     return sorted(influencers,key=vanity)
+
+
+# def bubble_sort(nums):
+#     swapping = True
+#     end = len(nums)
+#     while swapping:
+#         swapping = False
+#         for i in range(1, end):
+#             if nums[i - 1] > nums[i]:
+#                 temp = nums[i - 1]
+#                 nums[i - 1] = nums[i]
+#                 nums[i] = temp
+#                 swapping = True
+#         end -= 1
+#     return nums
+
+
+def merge_sort(nums):
+    pass
+
+
+def merge(first, second):
+    pass
