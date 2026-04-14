4a4855b5d5df480caa440621ae1783a19c45a309
Laptop HP <tienpham102vp@gmail.com>
2026-04-14 08:37:26 +0700

    add insertion_sort and quick_sort

--- projects/boot_dev/sorting_algo/main.py
+++ projects/boot_dev/sorting_algo/main.py
@@ -39,3 +39,35 @@ def merge_sort(nums):
 
 def merge(first, second):
     pass
+
+
+def insertion_sort(nums):
+    if nums is None:
+        return None
+    leng = len(nums)
+    for i in range(1, leng):
+        j = i
+        while j > 0 and nums[j - 1] > nums[j]:
+            temp = nums[j]
+            nums[j] = nums[j - 1]
+            nums[j - 1] = temp
+            j -= 1
+    return nums
+
+
+def quick_sort(nums, low, high):
+    if low < high:
+        middle = partition(nums, low, high)
+        quick_sort(nums, low, middle - 1)
+        quick_sort(nums, middle, high)
+
+
+def partition(nums, low, high):
+    pivot = nums[high]
+    i = low - 1
+    for j in range(low, high):
+        if nums[j] < pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+    nums[i + 1], nums[high] = nums[high], nums[i + 1]
+    return i + 1
