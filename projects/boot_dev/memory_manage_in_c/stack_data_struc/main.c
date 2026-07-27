2ab3341f4f048bba29b8c928c21b7954a1aa06bb
Laptop HP <tienpham102vp@gmail.com>
2026-07-27 23:39:59 +0700

    add 'add' function for snek obj

--- projects/boot_dev/memory_manage_in_c/stack_data_struc/main.c
+++ projects/boot_dev/memory_manage_in_c/stack_data_struc/main.c
@@ -53,11 +53,103 @@ int main(int argc, char *argv[]) {
   //   printf("fail: don't use calloc");
   // }
 
-  snek_object_t *obj = new_snek_array(2);
-  snek_object_t *first = new_snek_string("First");
-  snek_object_t *second = new_snek_integer(3);
-  snek_array_set(obj, 0, first);
-  snek_array_set(obj, 1, second);
-  printf("first: %s", obj->data.v_array.elements[0]->data.v_string);
-  printf("second: %d", snek_array_get(obj, 1)->data.v_int);
+  // snek_object_t *obj = new_snek_array(2);
+  // snek_object_t *first = new_snek_string("First");
+  // snek_object_t *second = new_snek_integer(3);
+  // snek_array_set(obj, 0, first);
+  // snek_array_set(obj, 1, second);
+  // printf("first: %s", obj->data.v_array.elements[0]->data.v_string);
+  // printf("second: %d", snek_array_get(obj, 1)->data.v_int);
+
+  // snek_object_t *one = new_snek_integer(1);
+  // printf("one data: %d\n",one->data.v_int);
+  // snek_object_t *three = new_snek_integer(3);
+  // printf("three data: %d\n",three->data.v_int);
+  // snek_object_t *four = snek_add(one, three);
+  // printf("four data: %d\n",four->data.v_int);
+  //
+  // printf("kind: %d\n",four->kind);
+  // printf("data: %d\n",four->data.v_int);
+
+  // snek_object_t *one = new_snek_float(1.5);
+  // printf("one data: %f\n",one->data.v_float);
+  // snek_object_t *three = new_snek_float(3.5);
+  // printf("three data: %f\n",three->data.v_float);
+  // snek_object_t *five = snek_add(one, three);
+  // printf("five data: %f\n",five->data.v_float);
+
+  // snek_object_t *hello = new_snek_string("hello");
+  // printf("%s\n",hello->data.v_string);
+  // snek_object_t *world = new_snek_string(", world");
+  // printf("%s\n",world->data.v_string);
+  // snek_object_t *greeting = snek_add(hello, world);
+  // printf("%s\n",greeting->data.v_string);
+
+  // snek_object_t *one = new_snek_float(1.0);
+  // snek_object_t *two = new_snek_float(2.0);
+  // snek_object_t *three = new_snek_float(3.0);
+  // snek_object_t *four = new_snek_float(4.0);
+  // snek_object_t *five = new_snek_float(5.0);
+  // snek_object_t *six = new_snek_float(6.0);
+  //
+  // snek_object_t *v1 = new_snek_vector3(one, two, three);
+  // snek_object_t *v2 = new_snek_vector3(four, five, six);
+  // snek_object_t *result = snek_add(v1, v2);
+  //
+  // printf("result {%f, %f, %f}", result->data.v_vector3.x->data.v_float,
+  //        result->data.v_vector3.y->data.v_float,
+  //        result->data.v_vector3.z->data.v_float);
+
+  // snek_object_t *one = new_snek_integer(1);
+  // snek_object_t *ones = new_snek_array(2);
+  // snek_array_set(ones, 0, one);
+  // snek_array_set(ones, 1, one);
+  // printf("first elem of ones obj: %d\n",
+  //        ones->data.v_array.elements[0]->data.v_int);
+  //
+  // snek_object_t *hi = new_snek_string("hi");
+  // snek_object_t *hellos = new_snek_array(3);
+  // snek_array_set(hellos, 0, hi);
+  // snek_array_set(hellos, 1, hi);
+  // snek_array_set(hellos, 2, hi);
+  // printf("first elem of hellos obj: %s\n",
+  //        hellos->data.v_array.elements[0]->data.v_string);
+  // snek_object_t *result = snek_add(ones, hellos);
+  // printf("first elem of result obj: %d\n",
+  //        result->data.v_array.elements[0]->data.v_int);
+  //
+  // printf("kind: %d\n", result->kind);
+  // printf("size: %zu\n", result->data.v_array.size);
+  //
+  // snek_object_t *first = snek_array_get(result, 0);
+  // printf("first should be 1: %d\n", first->data.v_int);
+  // snek_object_t *second = snek_array_get(result, 1);
+  // printf("second should be 1: %d\n", second->data.v_int);
+  // snek_object_t *third = snek_array_get(result, 2);
+  // printf("third should be string 'hi': %s\n", third->data.v_string);
+  // snek_object_t *fourth = snek_array_get(result, 3);
+  // printf("fourth should be string 'hi': %s\n", fourth->data.v_string);
+  // snek_object_t *fifth = snek_array_get(result, 4);
+  // printf("fifth should be string 'hi': %s\n", fifth->data.v_string);
+
+  snek_object_t *empty = new_snek_array(0);
+  snek_object_t *hi = new_snek_string("hi");
+  snek_object_t *hellos = new_snek_array(2);
+  snek_array_set(hellos, 0, hi);
+  snek_array_set(hellos, 1, hi);
+
+  snek_object_t *result = snek_add(empty, hellos);
+
+  printf("result kind: %d\n", result->kind);
+  printf("result size: %zu\n", result->data.v_array.size);
+  // assert_size(result->data.v_array.size, ==, 2,
+  // "result should have 2 elements");
+
+  snek_object_t *first = snek_array_get(result, 0);
+  printf("first obj data should be 'hi' : %s\n", first->data.v_string);
+  // assert_string_equal(first->data.v_string, "hi", "first == hi");
+
+  snek_object_t *second = snek_array_get(result, 1);
+  // assert_string_equal(second->data.v_string, "hi", "second == hi");
+  printf("second obj data should be 'hi' : %s\n", second->data.v_string);
 }
--- projects/boot_dev/memory_manage_in_c/stack_data_struc/snekobject.c
+++ projects/boot_dev/memory_manage_in_c/stack_data_struc/snekobject.c
@@ -1,9 +1,97 @@
 #include "snekobject.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+snek_object_t *snek_add(snek_object_t *a, snek_object_t *b) {
+  if (a == NULL || b == NULL) {
+    return NULL;
+  }
+  if (a->kind == INTERGER) {
+    if (b->kind == INTERGER) {
+      snek_object_t *c = new_snek_integer(a->data.v_int + b->data.v_int);
+      return c;
+    } else if (b->kind == FLOAT) {
+      snek_object_t *c = new_snek_float(a->data.v_int + b->data.v_float);
+      return c;
+    } else {
+      return NULL;
+    }
+  } else if (a->kind == FLOAT) {
+    if (b->kind == FLOAT || b->kind == INTERGER) {
+      snek_object_t *c = new_snek_float(a->data.v_float + b->data.v_float);
+      return c;
+    } else {
+      return NULL;
+    }
+
+  } else if (a->kind == STRING) {
+    if (b->kind != STRING) {
+      return NULL;
+    } else {
+      size_t c_size = strlen(a->data.v_string) + strlen(b->data.v_string);
+      char *c_str = calloc(c_size, 1);
+      strcat(c_str, a->data.v_string);
+      strcat(c_str, b->data.v_string);
+      snek_object_t *c = new_snek_string(c_str);
+      free(c_str);
+      return c;
+    }
+  } else if (a->kind == VECTOR3) {
+    if (b->kind != VECTOR3) {
+      return NULL;
+    }
+    snek_object_t *c_x = snek_add(a->data.v_vector3.x, b->data.v_vector3.x);
+    // printf("vector c_x : %f", c_x->data.v_float);
+    snek_object_t *c_y = snek_add(a->data.v_vector3.y, b->data.v_vector3.y);
+    // printf("vector c_y : %f", c_y->data.v_float);
+    snek_object_t *c_z = snek_add(a->data.v_vector3.z, b->data.v_vector3.z);
+    // printf("vector c_z : %f", c_z->data.v_float);
+    snek_object_t *c = new_snek_vector3(c_x, c_y, c_z);
+    return c;
+  } else if (a->kind == ARRAY) {
+    if (b->kind != ARRAY) {
+      return NULL;
+    } else {
+      size_t a_size = a->data.v_array.size;
+      size_t b_size = b->data.v_array.size;
+      size_t c_size = a_size + b_size;
+      snek_object_t *c = new_snek_array(c_size);
+      for (size_t i = 0; i < a_size; i++) {
+        snek_array_set(c, i, snek_array_get(a, i));
+      }
+      for (size_t i = a_size; i < c->data.v_array.size; i++) {
+        snek_array_set(c, i, snek_array_get(b, i - a_size));
+      }
+      return c;
+    }
+  } else {
+    return NULL;
+  }
+}
+int snek_length(snek_object_t *obj) {
+  if (obj == NULL) {
+    return -1;
+  }
+  if (obj->kind == INTERGER) {
+    return 1;
+  }
+  if (obj->kind == FLOAT) {
+    return 1;
+  }
+  if (obj->kind == STRING) {
+    return strlen(obj->data.v_string);
+  }
+  if (obj->kind == VECTOR3) {
+    return 3;
+  }
+  if (obj->kind == ARRAY) {
+    return obj->data.v_array.size;
+  }
+  return -1;
+}
 snek_object_t *snek_array_get(snek_object_t *snek_obj, size_t index) {
   if (snek_obj == NULL) {
     return NULL;
